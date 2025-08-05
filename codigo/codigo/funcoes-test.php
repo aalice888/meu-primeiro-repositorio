c4294dbd81586130fb3516b4436eedff671067e3
aalice888 <0000925462@senaimgaluno.com.br>
2025-08-05 11:22:51 -0300

    Create funcoes-test.php
    Cria explicação de testes para soma

---
+++ codigo/codigo/funcoes-test.php
@@ -0,0 +1,4 @@
+<?php
+// Para testar a função soma($a, $b), eu chamaria a função com diferentes pares de números,
+// como soma(2, 3) esperando o resultado 5, soma(-1, 1) esperando 0, etc.
+// Compararia os resultados esperados com os retornados para garantir que está funcionando corretamente.
